ae959edda4cc88fa54af0d1a7495a54969bca6f6
Alex Sweeney <ajsweeney2324@gmail.com>
2023-03-05 14:21:32 -0600

    praciticing creating an array of objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
-alert('Hello World');
 
-let favoriteFood = 'Pizza';
-document.write("Favorite Food: " + favoriteFood);
+
+let pokemonList = [
+  {
+    name: 'Charmeleon',
+    height: 1.1,
+    abilities: ['Blaze', 'Solar-Powered']
+  },   
+  {
+    name: 'Sandslash',
+    height: 1.0,
+    abilities: ['Sand-Veil', 'Sand-Rush']
+  },
+  {
+    name: 'Primeape',
+    height: 1.0,
+    abilities: ['Vital-Spirit', 'Anger-Point', 'Defiant']
+  } 
+];
 
