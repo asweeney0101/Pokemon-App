cb8630ba8fdaf93e3aa4ccb37d4b6a827da58326
Alex Sweeney <ajsweeney2324@gmail.com>
2023-03-05 21:51:48 -0600

    submitted our practice for loop and if statement

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,28 @@
 
 let pokemonList = [
   {
-    name: 'Charmeleon',
+    name: "Charmeleon",
     height: 1.1,
-    abilities: ['Blaze', 'Solar-Powered']
+    abilities: ["Blaze", "Solar-Powered"]
   },   
   {
-    name: 'Sandslash',
+    name: "Sandslash",
     height: 1.0,
-    abilities: ['Sand-Veil', 'Sand-Rush']
+    abilities: ["Sand-Veil", "Sand-Rush"]
   },
   {
-    name: 'Primeape',
+    name: "Primeape",
     height: 1.0,
-    abilities: ['Vital-Spirit', 'Anger-Point', 'Defiant']
+    abilities: ["Vital-Spirit", "Anger-Point", "Defiant"]
   } 
 ];
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")");
+ if (pokemonList[i].height > 1) {
+    document.write(" - wow, that's a big one!");
+ }
+ document.write("<br>");
+}
 
