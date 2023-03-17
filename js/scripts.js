17149fd892eba8eb6157c8713619df053b707257
Alex Sweeney <ajsweeney2324@gmail.com>
2023-03-16 19:35:54 -0500

    reuploaded the IIFE w/forEach()

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,46 @@
 
 
-let pokemonList = [
-  {
-    name: "Charmeleon",
-    height: 1.1,
-    abilities: ["Blaze", "Solar-Powered"]
-  },   
-  {
-    name: "Sandslash",
-    height: 1.0,
-    abilities: ["Sand-Veil", "Sand-Rush"]
-  },
-  {
-    name: "Primeape",
-    height: 1.0,
-    abilities: ["Vital-Spirit", "Anger-Point", "Defiant"]
-  } 
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Charmeleon",
+      height: 1.1,
+      abilities: ["Blaze", "Solar-Powered"]
+    },   
+    {
+      name: "Sandslash",
+      height: 1.0,
+      abilities: ["Sand-Veil", "Sand-Rush"]
+    },
+    {
+      name: "Primeape",
+      height: 1.0,
+      abilities: ["Vital-Spirit", "Anger-Point", "Defiant"]
+    } 
+  ]
 
+    function getAll () {
+      return pokemonList;
+    }
+    function add (pokemon) {
+      pokemonList.push(pokemon);
+    }
 
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")");
- if (pokemonList[i].height > 1) {
-    document.write(" - wow, that's a big one!");
- }
- document.write("<br>");
-}
+    return {
+      getAll: getAll,
+      add: add
+    }
 
+  })()   
+
+  function displayPokemon(pokemon) {
+    document.write(pokemon.name + " (Height: " + pokemon.height + ")");
+    if (pokemon.height > 1) {
+      document.write(" - wow, that's a big one! ");
+    }
+    document.write("<br>");
+  }
+
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    displayPokemon(pokemon);
+  });
